5b54202941cacb37415adf6dcb23c16cb639d8f4
WhoHim <534801304@qq.com>
2019-04-03 16:48:48 +0800

    feat：新增取消留座接口

--- src/main/java/com/whohim/library/com/whohim/library/com/whohim/springboot/web/UserController.java
+++ src/main/java/com/whohim/library/com/whohim/library/com/whohim/springboot/web/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,27 +34,45 @@ public class UserController {
     private StringRedisTemplate stringRedisTemplate;
 
     private static final String HVAE_SEAT = "1";
-
     /**
      * 午餐时间
      **/
     private static final String LUNCH_BREAK = "11:30-12:30";
-
     /**
      * 晚餐时间
      **/
     private static final String DINNER_TIME = "17:00-18:00";
-
     /**
      * 日期格式
      **/
     private static final String HOUR_MINNI = "HH:mm";
 
     private static final String BIND_SUCCESS = "ok";
+    /**
+     * 留座时间
+     **/
+    private static final int DATE_TIME = 0;
+    /**
+     * 图书证号
+     **/
+    private static final int USER_ID = 1;
+    /**
+     * openId
+     **/
+    private static final int OPEN_ID = 2;
+    /**
+     * 头像
+     **/
+    private static final int AVATARURL = 3;
+    /**
+     * 昵称
+     **/
+    private static final int NICKNAME = 4;
 
     @PostMapping("/markLeave/")
     @ResponseBody
     public ServerResponse postMarkLeave(User user) throws Exception {
+        /* 这个是卡号 */
         String userId = user.getUserId();
         String seat = user.getSeat();
         String openId = user.getOpenId();
@@ -61,47 +80,43 @@ public class UserController {
         String nickName = user.getNickName();
         String personal = DateTimeUtil.dateToStr(new Date(), "yyyy-MM-dd HH:mm:ss") + "," + userId + "," + openId + "," + avatarUrl + "," + nickName + ",";
 
-        if (StringUtils.isBlank(openId) || StringUtils.isBlank(avatarUrl)) {
-            return ServerResponse.createByErrorMessage("參數不能为空!");
+        if (StringUtils.isBlank(openId) || StringUtils.isBlank(avatarUrl) || StringUtils.isBlank(seat) || StringUtils.isBlank(userId)) {
+            return ServerResponse.createByErrorMessage("参数不能为空!");
         }
 
-        if (StringUtils.isBlank(seat) || StringUtils.isBlank(userId)) {
-            return ServerResponse.createByErrorMessage("座位号或学号不能为空！");
-        }
-        String dateTimeUserIdRedis = stringRedisTemplate.opsForValue().get(seat);
-
-        if (StringUtils.isBlank(dateTimeUserIdRedis)) {
+        String userInfo = stringRedisTemplate.opsForValue().get(seat);
+        if (StringUtils.isBlank(userInfo)) {
             if (stringRedisTemplate.opsForValue().get(userId) != null) {
                 if (HVAE_SEAT.equals(stringRedisTemplate.opsForValue().get(userId))
                         || user.getOpenId().equals(stringRedisTemplate.opsForValue().get(openId))) {
-                    return ServerResponse.createByErrorMessage("同学，一个人只能占一个位哦！");
+                    return ServerResponse.createByErrorMessage("同学，一个人只能留一个位哦！");
                 }
             }
             if (isInTime(LUNCH_BREAK, DateTimeUtil.dateToStr(new Date(), HOUR_MINNI))) {
                 //向redis里存入数据和设置缓存时间
                 stringRedisTemplate.opsForValue().set(seat, personal, 60 * 90, TimeUnit.SECONDS);
-                return ServerResponse.createBySuccessMessage("占座成功！");
+                return ServerResponse.createBySuccessMessage("留座成功！");
             }
             if (isInTime(DINNER_TIME, DateTimeUtil.dateToStr(new Date(), HOUR_MINNI))) {
                 //向redis里存入数据和设置缓存时间
                 stringRedisTemplate.opsForValue().set(seat, personal, 60 * 90, TimeUnit.SECONDS);
-                return ServerResponse.createBySuccessMessage("占座成功！");
+                return ServerResponse.createBySuccessMessage("留座成功！");
             }
             //向redis里存入数据和设置缓存时间
             stringRedisTemplate.opsForValue().set(seat, personal, 60 * 30, TimeUnit.SECONDS);
-            //用于判断此学号是否占两个座位
+            //用于判断此学号是否留两个座位
             stringRedisTemplate.opsForValue().set(userId, HVAE_SEAT, 60 * 30, TimeUnit.SECONDS);
-            return ServerResponse.createBySuccessMessage("占座成功！");
+            return ServerResponse.createBySuccessMessage("留座成功！");
         }
-        String[] durs = dateTimeUserIdRedis.split(",");
+        String[] detailInfo = userInfo.split(",");
 
-        if (durs[1].equals(userId)) {
-            return ServerResponse.createByErrorCodeMessage(2, "已经占座，请不要重复留座！");
+        if (detailInfo[USER_ID].equals(userId)) {
+            return ServerResponse.createByErrorCodeMessage(2, "已经留座，请不要重复留座！");
         }
         if (stringRedisTemplate.opsForValue().get(seat) != null) {
             return ServerResponse.createByErrorMessage("该座位有人！");
         }
-        return ServerResponse.createByErrorMessage("占座失败！");
+        return ServerResponse.createByErrorMessage("留座失败！");
     }
 
     @GetMapping("/markLeave/{seat}")
@@ -110,21 +125,43 @@ public class UserController {
         if (StringUtils.isBlank(stringRedisTemplate.opsForValue().get(seat))) {
             return ServerResponse.createBySuccessMessage("该座位没人！");
         }
-        String dateTimeUserId = stringRedisTemplate.opsForValue().get(seat);
+        String userInfo = stringRedisTemplate.opsForValue().get(seat);
         Long ttlTime = stringRedisTemplate.getExpire(seat);
         logger.info(String.valueOf(ttlTime / 60.0));
-        String[] strs = dateTimeUserId.split(",");
+        String[] detailInfo = userInfo.split(",");
         HashMap<String, String> res = new HashMap<String, String>(6, 1) {
             {
-                put("Datetime", strs[0]);
-                put("userId", strs[1]);
-                put("openId", strs[2]);
-                put("avatarUrl", strs[3]);
-                put("nickName", strs[4]);
+                put("Datetime", detailInfo[DATE_TIME]);
+                put("userId", detailInfo[USER_ID]);
+                put("openId", detailInfo[OPEN_ID]);
+                put("avatarUrl", detailInfo[AVATARURL]);
+                put("nickName", detailInfo[NICKNAME]);
             }
         };
         return ServerResponse.createByErrorMessage("该座位有人！", res);
     }
+
+    @PostMapping("/canceSeat/")
+    @ResponseBody
+    public ServerResponse cancelSeat(@RequestParam("seat") String seat, @RequestParam("openId") String openId) {
+        if (StringUtils.isBlank(seat) || StringUtils.isBlank(openId)) {
+            return ServerResponse.createByErrorMessage("参数不能为空!");
+        }
+        String userInfo = stringRedisTemplate.opsForValue().get(seat);
+        if (StringUtils.isBlank(userInfo)) {
+            return ServerResponse.createByErrorMessage("同学，你没有留座！");
+        }
+        String[] detailInfo = userInfo.split(",");
+        if (!detailInfo[OPEN_ID].equals(openId)) {
+            return ServerResponse.createByErrorMessage("传错参了哦！");
+        }
+
+         /* 取消留座逻辑 */
+        stringRedisTemplate.delete(detailInfo[USER_ID]);
+        stringRedisTemplate.delete(seat);
+        return ServerResponse.createBySuccessMessage("取消留座成功！");
+    }
+
 
     @PostMapping("/user/bindLibrary")
     @ResponseBody
